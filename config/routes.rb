21a99fb5e59335681048634d46c6ed921d4cee38
Steven Huang <szteven3@gmail.com>
2014-02-22 10:44:34 +0800

    set hello as homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 ThirdApp::Application.routes.draw do
+
+  get "hello" => "hello#index"
+
+  root to:"hello#index"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
